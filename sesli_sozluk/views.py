56d6011a6772bd8a3fa0ddd3beb5c4f0ba158593
Can Yilmaz <can@potatolondon.com>
2012-11-12 23:59:55 +0000

    Returning translations for each key as a list

--- sesli_sozluk/views.py
+++ sesli_sozluk/views.py
@@ -1,25 +1,31 @@
-import urllib2
+import urllib2, re
 from django.http import HttpResponse
 from bs4 import BeautifulSoup
 from django.utils import simplejson
+
 
 def parse_response(self, word):
 
     url = 'http://m.seslisozluk.net/?word=%s' % word
     response = urllib2.urlopen(url)
 
-    soup = BeautifulSoup(response.read())
+    soup = BeautifulSoup(response.read(), from_encoding='utf-8')
 
     results = soup.findAll('div', {'class': 'resultset'})
 
     translations = []
+    pattern = r'\d{1,3}\.\s([\w\s!,;()\X]+)+'
 
     for result in results:
         title = result.find('div', { 'class': 'top' })
         translation = result.find('tbody')
-        translations.append({
-            'title': title.text,
-            'content': translation.text
-        })
+        matches = re.findall(pattern, unicode(translation.text), re.UNICODE)
+
+        if matches:
+            translations.append({
+                'title': title.text,
+                'translations': matches
+            })
+    print response.headers['content-type']
 
     return HttpResponse(simplejson.dumps(translations), content_type="application/json")
